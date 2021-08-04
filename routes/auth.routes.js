c6373c986667ab7039c5bf8f7abf81021efd51f9
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-08-04 16:14:35 +0200

    server-side validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
+const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
 const bcrypt = require('bcryptjs');
+const { reset } = require("nodemon");
 const saltRounds = 10;
 
 
@@ -18,6 +20,24 @@ router.get('/signup', (req, res) => {
 router.post('/signup', (req, res) => {
     const { username, email, password } = req.body;
 
+    // Check if all fields provided
+    if( !username || !email || !password ) {
+        const missingFields = {
+            username: !username,
+            email: !email,
+            password: !password,
+        }
+        res.render("auth/signup", { errorMessage: "Please provide in all fields.", missingFields})
+    }
+
+    // Check if password is strong enough
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+      res.status(400).render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+      return;
+    }
+
+
     bcrypt.genSalt(saltRounds)
         .then( salt => {
             return bcrypt.hash(password, salt);
@@ -28,6 +48,20 @@ router.post('/signup', (req, res) => {
         .then( userFromDB => {
             res.redirect('/userProfile')
         })
+        .catch( error => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                // Mongoose validation error
+                res.status(400).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000) {
+                // MongoDB validation error
+                res.status(400).render('auth/signup', {
+                   errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+                });
+            } else {
+                console.log(error)
+                next(error);
+            }
+        });
 });
 
 module.exports = router;
